cd3b7086b3d3a5847b522610c4bfc94aa9ed93b5
AIIexandra <9046313266@mail.ru>
2022-03-25 20:56:57 +0300

    Open cell

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -10,12 +10,16 @@ public class MainScript : MonoBehaviour
     [SerializeField] int yCount = 9;
     [SerializeField] int mineCount = 15;
     [SerializeField] GameObject chunckUp;
+    //[SerializeField] GameObject panel;
 
     int[,] grid;
+
+    GameObject[,] gameObjects;
 
     void Start()
     {
         grid = new int[xCount, yCount];
+        gameObjects = new GameObject[xCount, yCount];
 
         for (int i = 0; i < xCount; i++)
         {
@@ -52,8 +56,9 @@ public class MainScript : MonoBehaviour
         {
             for (int j = 0; j < yCount; j++)
             {
-                GameObject obj = Instantiate(chunckUp, Vector3.zero, Quaternion.identity, transform);
-                RectTransform rt = obj.GetComponent<RectTransform>();
+                gameObjects[i, j] = Instantiate(chunckUp, Vector3.zero, Quaternion.identity, transform);
+                RectTransform rt = gameObjects[i, j].GetComponent<RectTransform>();
+
                 rt.anchorMin = new Vector2(i * 1.0f / xCount, j * 1.0f / yCount);
                 rt.anchorMax = new Vector2((i + 1) * 1.0f / xCount, (j + 1) * 1.0f / yCount);
                 rt.offsetMin = Vector2.zero;
@@ -63,12 +68,34 @@ public class MainScript : MonoBehaviour
                 if (grid[i, j] == 0) chunckText.text = "";
                 if (grid[i, j] < 0) chunckText.text = "*";
                 if (grid[i, j] > 0) chunckText.text = grid[i, j].ToString();
+                gameObjects[i, j].SetActive(false);
             }
         }
     }
 
-    void Update()
+    public void OnPointerDown(BaseEventData data)
     {
-        
+        PointerEventData pointerEventData = (PointerEventData)data;
+        int i = (int)(pointerEventData.position.x / Screen.width * xCount);
+        int j = (int)(pointerEventData.position.y / Screen.height * yCount);
+        OpenChunk(i, j);
+    }
+
+    void OpenChunk(int i, int j)
+    {
+        if (gameObjects[i, j].activeSelf) return;
+        gameObjects[i, j].SetActive(true);
+
+        if (grid[i, j] == 0)
+        {
+            if (i > 0) OpenChunk(i - 1, j);
+            if (j > 0) OpenChunk(i, j - 1);
+            if (i < xCount - 1) OpenChunk(i + 1, j);
+            if (j < yCount - 1) OpenChunk(i, j + 1);
+            if (i > 0 && j > 0) OpenChunk(i - 1, j - 1);
+            if (i > 0 && j < yCount - 1) OpenChunk(i - 1, j + 1);
+            if (i < xCount - 1 && j > 0) OpenChunk(i + 1, j - 1);
+            if (i < xCount - 1 && j < yCount - 1) OpenChunk(i + 1, j + 1);
+        }
     }
 }
