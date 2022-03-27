f2a966194a4f4894bfc43eb4c1d84457a1fff748
AIIexandra <9046313266@mail.ru>
2022-03-27 18:22:48 +0300

    Chunk up and chunk down

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -9,17 +9,40 @@ public class MainScript : MonoBehaviour
     [SerializeField] int xCount = 9;
     [SerializeField] int yCount = 9;
     [SerializeField] int mineCount = 15;
-    [SerializeField] GameObject chunckUp;
-    //[SerializeField] GameObject panel;
-
+    [SerializeField] GameObject chunkUp;
+    [SerializeField] GameObject chunkDown;
+    
     int[,] grid;
+    GameObject[,] chunksUp;
+    GameObject[,] chunksDown;
 
-    GameObject[,] gameObjects;
+    [SerializeField] RectTransform rectTransformPanel;
+    [SerializeField] int margin = 10;
+    float widthPanel;
+    float heightPanel;
 
     void Start()
     {
+        //размеры панели
+        //rectTransformPanel = panel.GetComponent<RectTransform>();
+
+        float top = (Screen.height - Screen.width) * 0.5f + margin;
+        float left = margin;
+        rectTransformPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0.0f, 1060);
+        rectTransformPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0.0f, 1060);
+        rectTransformPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0.0f, 1060);
+        rectTransformPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0.0f, 1060);
+
+        rectTransformPanel.localPosition = new Vector2(0, 0);
+
+        heightPanel = Screen.height - top * 2;
+        widthPanel = Screen.width - left * 2;
+        Debug.Log("высота: " + heightPanel + ", ширина: " + widthPanel);
+
+        //генерация ячеек
         grid = new int[xCount, yCount];
-        gameObjects = new GameObject[xCount, yCount];
+        chunksDown = new GameObject[xCount, yCount];
+        chunksUp = new GameObject[xCount, yCount];
 
         for (int i = 0; i < xCount; i++)
         {
@@ -56,8 +79,8 @@ public class MainScript : MonoBehaviour
         {
             for (int j = 0; j < yCount; j++)
             {
-                gameObjects[i, j] = Instantiate(chunckUp, Vector3.zero, Quaternion.identity, transform);
-                RectTransform rt = gameObjects[i, j].GetComponent<RectTransform>();
+                chunksDown[i, j] = Instantiate(chunkDown, Vector3.zero, Quaternion.identity, transform);
+                RectTransform rt = chunksDown[i, j].GetComponent<RectTransform>();
 
                 rt.anchorMin = new Vector2(i * 1.0f / xCount, j * 1.0f / yCount);
                 rt.anchorMax = new Vector2((i + 1) * 1.0f / xCount, (j + 1) * 1.0f / yCount);
@@ -65,10 +88,38 @@ public class MainScript : MonoBehaviour
                 rt.offsetMax = Vector2.zero;
 
                 Text chunckText = rt.GetChild(0).GetComponent<Text>();
-                if (grid[i, j] == 0) chunckText.text = "";
-                if (grid[i, j] < 0) chunckText.text = "*";
-                if (grid[i, j] > 0) chunckText.text = grid[i, j].ToString();
-                gameObjects[i, j].SetActive(false);
+                Image mine = rt.GetChild(1).GetComponent<Image>();
+
+                if (grid[i, j] == 0) 
+                { 
+                    chunckText.text = "";
+                    mine.enabled = false;
+                }
+
+                if (grid[i, j] < 0)
+                {
+                    chunckText.text = "";
+                    mine.enabled = true;
+                }
+
+                if (grid[i, j] > 0)
+                {
+                    chunckText.text = grid[i, j].ToString();
+                    mine.enabled = false;
+                }
+
+                chunksUp[i, j] = Instantiate(chunkUp, Vector3.zero, Quaternion.identity, transform);
+                rt = chunksUp[i, j].GetComponent<RectTransform>();
+
+                rt.anchorMin = new Vector2(i * 1.0f / xCount, j * 1.0f / yCount);
+                rt.anchorMax = new Vector2((i + 1) * 1.0f / xCount, (j + 1) * 1.0f / yCount);
+                rt.offsetMin = Vector2.zero;
+                rt.offsetMax = Vector2.zero;
+
+                Image flag = rt.GetChild(0).GetComponent<Image>();
+                Image question = rt.GetChild(1).GetComponent<Image>();
+                flag.enabled = false;
+                question.enabled = false;
             }
         }
     }
@@ -76,26 +127,56 @@ public class MainScript : MonoBehaviour
     public void OnPointerDown(BaseEventData data)
     {
         PointerEventData pointerEventData = (PointerEventData)data;
-        int i = (int)(pointerEventData.position.x / Screen.width * xCount);
-        int j = (int)(pointerEventData.position.y / Screen.height * yCount);
+        int i = (int)((pointerEventData.position.x / widthPanel * xCount));
+        int j = (int)((pointerEventData.position.y - 430) / heightPanel * yCount);
         OpenChunk(i, j);
+        Debug.Log("[" + i + ", " + j + "]");
     }
 
     void OpenChunk(int i, int j)
     {
-        if (gameObjects[i, j].activeSelf) return;
-        gameObjects[i, j].SetActive(true);
-
-        if (grid[i, j] == 0)
+        if (chunksUp[i, j].activeSelf)
         {
-            if (i > 0) OpenChunk(i - 1, j);
-            if (j > 0) OpenChunk(i, j - 1);
-            if (i < xCount - 1) OpenChunk(i + 1, j);
-            if (j < yCount - 1) OpenChunk(i, j + 1);
-            if (i > 0 && j > 0) OpenChunk(i - 1, j - 1);
-            if (i > 0 && j < yCount - 1) OpenChunk(i - 1, j + 1);
-            if (i < xCount - 1 && j > 0) OpenChunk(i + 1, j - 1);
-            if (i < xCount - 1 && j < yCount - 1) OpenChunk(i + 1, j + 1);
+            chunksUp[i, j].SetActive(false);
+
+            if (grid[i, j] == 0)
+            {
+                if (i > 0) OpenChunk(i - 1, j);
+                if (j > 0) OpenChunk(i, j - 1);
+                if (i < xCount - 1) OpenChunk(i + 1, j);
+                if (j < yCount - 1) OpenChunk(i, j + 1);
+                if (i > 0 && j > 0) OpenChunk(i - 1, j - 1);
+                if (i > 0 && j < yCount - 1) OpenChunk(i - 1, j + 1);
+                if (i < xCount - 1 && j > 0) OpenChunk(i + 1, j - 1);
+                if (i < xCount - 1 && j < yCount - 1) OpenChunk(i + 1, j + 1);
+            }
+        }
+    }
+
+    public void onPress(BaseEventData data)
+    {
+        PointerEventData pointerEventData = (PointerEventData)data;
+        int i = (int)((pointerEventData.position.x / widthPanel * xCount));
+        int j = (int)((pointerEventData.position.y - 430) / heightPanel * yCount);
+
+        RectTransform rt = chunksUp[i, j].GetComponent<RectTransform>();
+        Image flag = rt.GetChild(0).GetComponent<Image>();
+        Image question = rt.GetChild(1).GetComponent<Image>();
+
+        if (!flag.enabled && !question.enabled)
+        {
+            flag.enabled = true;
+        }
+
+        else if (flag.enabled)
+        {
+            flag.enabled = false;
+            question.enabled = true;
+        }
+        else
+        {
+            flag.enabled = false;
+            question.enabled = false;
         }
     }
 }
