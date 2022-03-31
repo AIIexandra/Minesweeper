9ba91b3ff723beaf652c8a5e2dd7291273df0169
AIIexandra <9046313266@mail.ru>
2022-03-31 21:04:36 +0300

    Switch scenes

---
+++ Assets/Scripts/SceneSwitching.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    public void ToGame()
+    {
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void ToSelectLevel()
+    {
+        SceneManager.LoadSceneAsync("SelectLevelScene");
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadSceneAsync("MenuScene");
+    }
+
+    public void ToRules()
+    {
+        SceneManager.LoadSceneAsync("RulesScene");
+    }
+
+    public void ToSituations()
+    {
+        SceneManager.LoadSceneAsync("SituationsScene");
+    }
+}
